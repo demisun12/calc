f739445a4c81eb3565418e52afa31ca1646711ae
Sonal Suralikal <115199260+demisun12@users.noreply.github.com>
2024-07-04 14:25:55 -0700

    Wrote operate function
    Created function, operate, that takes an operator and 2 numbers and calls one of the previously created functions

--- js.js
+++ js.js
@@ -17,3 +17,15 @@ function divide(a,b) {
 let firstNumber = ;
 let secondNumber = ;
 let operator = ;
+
+function operate(firstNumber,secondNumber,operator) {
+    if (operator === "+") {
+        add(firstNumber,secondNumber);
+    } else if (operator === "-") {
+        subtract(firstNumber,secondNumber);
+    } else if (operator === "*") {
+        multiply(firstNumber,secondNumber);
+    } else (operator === "/") {
+        divide(firstNumber,secondNumber);
+    }
+}
